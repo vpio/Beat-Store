5fc87d3b8f25f2a7a8cee2f85c30369c8e51f52a
Pio Molina <vpio@comcast.net>
2019-04-12 16:22:25 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import firebase from "firebase";
 import logo from './logo.svg';
 import './App.css';
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBlTHNVntwcV87Hr62sk__-uRpD0qaDjTY",
+  authDomain: "beat-store-e5015.firebaseapp.com",
+  databaseURL: "https://beat-store-e5015.firebaseio.com",
+  projectId: "beat-store-e5015",
+  storageBucket: "beat-store-e5015.appspot.com",
+  messagingSenderId: "562866262962"
+};
+firebase.initializeApp(config);
+
 class App extends Component {
+  state = {
+    beats: null
+  }
+
+  componentDidMount() {
+    var storage = firebase.storage();
+    var pathReference = storage.ref('beats/');
+
+    pathReference.getDownloadURL().then(function(url){
+      this.setState({ beats: url })
+    }).catch(function(error) { alert('could not get file! Submit feedback to github.com/vpio') })
+  }
+
   render() {
     return (
       <div className="App">
